c7f62aaa18679f2d12c037532f9d541d8c54f197
Rupan Samanta <96118309+RupanSamanta@users.noreply.github.com>
2022-09-05 01:34:19 +0530

    Update script.js

--- calculator/script.js
+++ calculator/script.js
@@ -5,7 +5,7 @@ numericKey = [
 			key[4], key[5], key[6], key[8], key[9],
 			key[10], key[12],	key[13], key[14], key[17],
 			key[16]
-],
+], 
 operatorKey = [
 		 key[1], 	key[2], key[3], key[7], key[11],
 		 key[15], key[20], key[21], key[22], key[23],
