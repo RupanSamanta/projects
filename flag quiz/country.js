a6d548dc13d160df5f3b4557ba77681541d78ef3
Rupan Samanta <96118309+RupanSamanta@users.noreply.github.com>
2022-09-29 08:36:18 +0530

    Add files via upload

---
+++ flag quiz/country.js
@@ -0,0 +1,213 @@
+country = [
+{
+    name: "Afghanistan";
+    capital: "Kabul";
+    continent: "Asia";
+},
+{
+    name: "Albania";
+    capital: "Tirana";
+    continent: "Europe";
+},
+{
+    name: "Algeria";
+    capital: "Algiers";
+    continent: "Africa";
+},
+{
+    name: "Andorra";
+    capital: "Andorra La Vella";
+    continent: ;
+},
+{
+    name: "Angola";
+    capital: "Luanda";
+    continent: ;
+},
+{
+    name: "Antigua & Barbuda";
+    capital: "Saint John's" ;
+    continent: ;
+},
+{
+    name: "Argentina";
+    capital: "Buenos Aires" ;
+    continent: "South America";
+},
+{
+    name: "Armenia";
+    capital: "Yerevan";
+    continent: "";
+},
+{
+    name: "Australia";
+    capital: "Canberra";
+    continent: "";
+},
+{
+    name: "Austria";
+    capital: "Vienna";
+    continent: "";
+},
+{
+    name: "Azerbaijan";
+    capital: "Baku";
+    continent: "";
+},
+{
+    name: "Bahamas";
+    capital: "Nassau";
+    continent: "";
+},
+{
+    name: "Bahrain";
+    capital: "Manama";
+    continent: "";
+},
+{
+    name: "Bangladesh";
+    capital: "Dhaka";
+    continent: "";
+},
+{
+    name: "Barbados";
+    capital: "Bridgetown";
+    continent: "";
+},
+{
+    name: "Belarus";
+    capital: "Minsk";
+    continent: "";
+},
+{
+    name: "Belgium";
+    capital: "Brussels";
+    continent: "";
+},
+{
+    name: "Belize";
+    capital: "Belmopan";
+    continent: "";
+},
+{
+    name: "Benin";
+    capital: "Porto-Novo";
+    continent: "";
+},
+{
+    name: "Bhutan";
+    capital: "Thimphu";
+    continent: "";
+},
+{
+    name: "Bolivia";
+    capital: "Sucre";
+    continent: "";
+},
+{
+    name: "Bosnia and Herzegovina";
+    capital: "Sarajevo";
+    continent: "";
+},
+{
+    name: "Botswana";
+    capital: "Gaborone";
+    continent: "";
+},
+{
+    name: "Brazil";
+    capital: "Brasilia";
+    continent: "";
+},
+{
+    name: "Brunei";
+    capital: "Bandar Seri Begawan";
+    continent: "";
+},
+{
+    name: "Bulgaria";
+    capital: "Sofia";
+    continent: "";
+},
+{
+    name: "Burkina Faso";
+    capital: "Ouagadougou";
+    continent: "";
+},
+{
+    name: "Burundi";
+    capital: "Gitega";
+    continent: "";
+},
+{
+    name: "Cambo Verde";
+    capital: "Praia";
+    continent: "";
+},
+{
+    name: "Cambodia";
+    capital: "Phnom Penh";
+    continent: "";
+},
+{
+    name: "Cameroon";
+    capital: "Phnom Pneh";
+    continent: "";
+},
+{
+    name: "Cameroon";
+    capital: "Yaounde";
+    continent: "";
+},
+{
+    name: "Canada";
+    capital: "Ottawa";
+    continent: "";
+},
+{
+    name: "Central African Republic";
+    capital: "Bangui";
+    continent: "";
+},
+{
+    name: "Chad";
+    capital: "N'Djamena";
+    continent: "";
+},
+{
+    name: "Chile";
+    capital: "Santiago";
+    continent: "";
+},
+{
+    name: "China";
+    capital: "Beijing";
+    continent: "";
+},
+{
+    name: "Colombia";
+    capital: "Bogota";
+    continent: "";
+},
+{
+    name: "Comoros";
+    capital: "Moroni";
+    continent: "";
+},
+{
+    name: "D. R. Congo";
+    capital: "Kinshasa";
+    continent: "";
+},
+{
+    name: "Republic of Congo";
+    capital: "Brazzaville";
+    continent: "";
+},
+{
+    name: "Costa Rica";
+    capital: "San Jose";
+    continent: "";
+},
+
+]
