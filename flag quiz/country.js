8f4f0e33d58085b80b728986b9303064d9bf1237
Rupan Samanta <96118309+RupanSamanta@users.noreply.github.com>
2022-09-29 18:57:03 +0530

    Add files via upload

---
+++ flag quiz/country.js
@@ -0,0 +1,982 @@
+country = [
+{
+    name: "Afghanistan",
+    capital: "Kabul",
+    continent: "Asia"
+},
+{
+    name: "Albania",
+    capital: "Tirana",
+    continent: "Europe"
+},
+{
+    name: "Algeria",
+    capital: "Algiers",
+    continent: "Africa"
+},
+{
+    name: "Andorra",
+    capital: "Andorra La Vella",
+    continent: "Europe"
+},
+{
+    name: "Angola",
+    capital: "Luanda",
+    continent: "Africa"
+},
+{
+    name: "Antigua & Barbuda",
+    capital: "Saint John's" ,
+    continent: "North America"
+},
+{
+    name: "Argentina",
+    capital: "Buenos Aires" ,
+    continent: "South America"
+},
+{
+    name: "Armenia",
+    capital: "Yerevan",
+    continent: "Asia"
+},
+{
+    name: "Australia",
+    capital: "Canberra",
+    continent: "Oceania"
+},
+{
+    name: "Austria",
+    capital: "Vienna",
+    continent: "Europe"
+},
+{
+    name: "Azerbaijan",
+    capital: "Baku",
+    continent: "Asia, Europe"
+},
+{
+    name: "Bahamas",
+    capital: "Nassau",
+    continent: "North America"
+},
+{
+    name: "Bahrain",
+    capital: "Manama",
+    continent: "Asia"
+},
+{
+    name: "Bangladesh",
+    capital: "Dhaka",
+    continent: "Asia"
+},
+{
+    name: "Barbados",
+    capital: "Bridgetown",
+    continent: "North America"
+},
+{
+    name: "Belarus",
+    capital: "Minsk",
+    continent: "Europe"
+},
+{
+    name: "Belgium",
+    capital: "Brussels",
+    continent: "Europe"
+},
+{
+    name: "Belize",
+    capital: "Belmopan",
+    continent: "North America"
+},
+{
+    name: "Benin",
+    capital: "Porto-Novo",
+    continent: "Africa"
+},
+{
+    name: "Bhutan",
+    capital: "Thimphu",
+    continent: "Asia"
+},
+{
+    name: "Bolivia",
+    capital: "Sucre",
+    continent: "South America"
+},
+{
+    name: "Bosnia and Herzegovina",
+    capital: "Sarajevo",
+    continent: "Europe"
+},
+{
+    name: "Botswana",
+    capital: "Gaborone",
+    continent: "Africa"
+},
+{
+    name: "Brazil",
+    capital: "Brasilia",
+    continent: "Soth America"
+},
+{
+    name: "Brunei",
+    capital: "Bandar Seri Begawan",
+    continent: "Asia"
+},
+{
+    name: "Bulgaria",
+    capital: "Sofia",
+    continent: "Europe"
+},
+{
+    name: "Burkina Faso",
+    capital: "Ouagadougou",
+    continent: "Africa"
+},
+{
+    name: "Burundi",
+    capital: "Gitega",
+    continent: "Africa"
+},
+{
+    name: "Cambodia",
+    capital: "Phnom Penh",
+    continent: "Asia"
+},
+{
+    name: "Cameroon",
+    capital: "Yaounde",
+    continent: "Africa"
+},
+{
+    name: "Canada",
+    capital: "Ottawa",
+    continent: "North Africa"
+},
+{
+    name: "Cape Verde",
+    capital: "Praia",
+    continent: "Africa"
+},
+{
+    name: "Central African Republic",
+    capital: "Bangui",
+    continent: "Africa"
+},
+{
+    name: "Chad",
+    capital: "N'Djamena",
+    continent: "Africa"
+},
+{
+    name: "Chile",
+    capital: "Santiago",
+    continent: "South America"
+},
+{
+    name: "China",
+    capital: "Beijing",
+    continent: "Asia"
+},
+{
+    name: "Colombia",
+    capital: "Bogota",
+    continent: "South America"
+},
+{
+    name: "Comoros",
+    capital: "Moroni",
+    continent: "Africa"
+},
+{
+    name: "D. R. Congo",
+    capital: "Kinshasa",
+    continent: "Africa"
+},
+{
+    name: "Republic of Congo",
+    capital: "Brazzaville",
+    continent: "Africa"
+},
+{
+    name: "Costa Rica",
+    capital: "San Jose",
+    continent: "North America"
+},
+{
+    name: "Croatia",
+    capital: "Zagreb",
+    continent: "Europe"
+},
+{
+    name: "Cuba",
+    capital: "Havana",
+    continent: "North America"
+},
+{
+    name: "Cyprus",
+    capital: "",
+    continent: "Asia"
+},
+{
+    name: "Czech Republic",
+    capital: "Prague",
+    continent: "Europe"
+},
+{
+    name: "Demnark",
+    capital: "Copenhagen",
+    continent: "Europe"
+},
+{
+    name: "Djibouti",
+    capital: "Djibouti",
+    continent: "Africa"
+},
+{
+    name: "Dominica",
+    capital: "Roseau",
+    continent: "North America"
+},
+{
+    name: "Dominican Republic",
+    capital: "Santo Domingo",
+    continent: "North America"
+},
+{
+    name: "East Timor",
+    capital: "",
+    continent: "Asia"
+},
+{
+    name: "Equador",
+    capital: "",
+    continent: "Africa"
+},
+{
+    name: "Egypt",
+    capital: "Cairo",
+    continent: "Africa"
+},
+{
+    name: "El Salvatore",
+    capital: "San Salvatore",
+    continent: "North America"
+},
+{
+    name: "Equatorial Guinea",
+    capital: "Malabo",
+    continent: "Africa"
+},
+{
+    name: "Eritrea",
+    capital: "Asmara",
+    continent: "Africa"
+},
+{
+    name: "Estonia",
+    capital: "Tallinn",
+    continent: "Europe"
+},
+{
+    name: "Eswatini",
+    capital: "Mbabane",
+    continent: "Africa"
+},
+{
+    name: "Ethiopia",
+    capital: "Addis Ababa",
+    continent: "Africa"
+},
+{
+    name: "Fiji",
+    capital: "Suva",
+    continent: "Oceania"
+},
+{
+    name: "Finland",
+    capital: "Helsinki",
+    continent: "Europe"
+},
+{
+    name: "France",
+    capital: "Paris",
+    continent: "Europe"
+},
+{
+    name: "Gabon",
+    capital: "Libreville",
+    continent: "Africa"
+},
+{
+    name: "Gambia",
+    capital: "Banjul",
+    continent: "Africa"
+},
+{
+    name: "Georgia",
+    capital: "Tbilisi",
+    continent: "Asia, Europe"
+},
+{
+    name: "Germany",
+    capital: "Berlin",
+    continent: "Europe"
+},
+{
+    name: "Ghana",
+    capital: "Accra",
+    continent: "Africa"
+},
+{
+    name: "Greece",
+    capital: "Athens",
+    continent: "Europe"
+},
+{
+    name: "Grenada",
+    capital: "Saint George's",
+    continent: "North America"
+},
+{
+    name: "Guatemala",
+    capital: "Guatemala City",
+    continent: "North America"
+},
+{
+    name: "Guinea",
+    capital: "Conakry",
+    continent: "Africa"
+},
+{
+    name: "Guinea-Bissau",
+    capital: "Bissau",
+    continent: "Africa"
+},
+{
+    name: "Guyana",
+    capital: "Georgetown",
+    continent: "South America"
+},
+{
+    name: "Haiti",
+    capital: "Port-au-Prince",
+    continent: "North America"
+},
+{
+    name: "Honduras",
+    capital: "Tegucigalpa",
+    continent: "North America"
+},
+{
+    name: "Hungary",
+    capital: "Budapest",
+    continent: "Europe"
+},
+{
+    name: "Iceland",
+    capital: "Reykjavík",
+    continent: "Europe"
+},
+{
+    name: "India",
+    capital: "New Delhi",
+    continent: "Asia"
+},
+{
+    name: "Indonesia",
+    capital: "Bali",
+    continent: "Asia"
+},
+{
+    name: "Iran",
+    capital: "Tehran",
+    continent: "Asia"
+},
+{
+    name: "Iraq",
+    capital: "Baghdad",
+    continent: "Asia"
+},
+{
+    name: "Ireland",
+    capital: "Dublin",
+    continent: "Europe"
+},
+{
+    name: "Israel",
+    capital: "Jerusalem",
+    continent: "Asia"
+},
+{
+    name: "Italy",
+    capital: "Rome",
+    continent: "Europe"
+},
+{
+    name: "Ivory Coast",
+    capital: "Yamoussoukro",
+    continent: "Africa"
+},
+{
+    name: "Jamaica",
+    capital: "Kingston",
+    continent: "North America"
+},
+{
+    name: "Japan",
+    capital: "Tokyo",
+    continent: "Asia"
+},
+{
+    name: "Jordan",
+    capital: "Amman",
+    continent: "Asia"
+},
+{
+    name: "Kazakhstan",
+    capital: "Astana",
+    continent: "Asia"
+},
+{
+    name: "Kenya",
+    capital: "Nairobi",
+    continent: "Africa"
+},
+{
+    name: "Kiribati",
+    capital: "South Tarawa",
+    continent: "Oceania"
+},
+{
+    name: "North Korea",
+    capital: "Pyongyang",
+    continent: "Asia"
+},
+{
+    name: "South Korea",
+    capital: "Seoul",
+    continent: "Asia"
+},
+{
+    name: "Kuwait",
+    capital: "Kuwait City",
+    continent: "Asia"
+},
+{
+    name: "Kyrgyzstan",
+    capital: "Bishkek",
+    continent: "Asia"
+},
+{
+    name: "Laos",
+    capital: "Vientiane",
+    continent: "Asia"
+},
+{
+    name: "Latvia",
+    capital: "Riga",
+    continent: "Europe"
+},
+{
+    name: "Lebanon",
+    capital: "Berut",
+    continent: "Asia"
+},
+{
+    name: "Lesotho",
+    capital: "Maseru",
+    continent: "Africa"
+},
+{
+    name: "Liberia",
+    capital: "Monrovia",
+    continent: "Africa"
+},
+{
+    name: "Libya",
+    capital: "Tripoli",
+    continent: "Africa"
+},
+{
+    name: "Liechtenstein",
+    capital: "Vaduz",
+    continent: "Europe"
+},
+{
+    name: "Lithuania",
+    capital: "Vilnius",
+    continent: "Europe"
+},
+{
+    name: "Luxembourg",
+    capital: "Luxembourg",
+    continent: "Europe"
+},
+{
+    name: "Madagascar",
+    capital: "Antananarivo",
+    continent: "Africa"
+},
+{
+    name: "Malawi",
+    capital: "Lilongwe",
+    continent: "Africa"
+},
+{
+    name: "Malaysia",
+    capital: "Kuala Lumpur",
+    continent: "Asia"
+},
+{
+    name: "Maldives",
+    capital: "Malé",
+    continent: "Asia"
+},
+{
+    name: "Mali",
+    capital: "Bamako",
+    continent: "Africa"
+},
+{
+    name: "Malta",
+    capital: "Valletta",
+    continent: "Europe"
+},
+{
+    name: "Marshall Islands",
+    capital: "Majuro",
+    continent: "Oceania"
+},
+{
+    name: "Mauritania",
+    capital: "Nouakchott",
+    continent: "Africa"
+},
+{
+    name: "Mauritius",
+    capital: "Port Louis",
+    continent: "Africa"
+},
+{
+    name: "Mexico",
+    capital: "Mexico City",
+    continent: "North America"
+},
+{
+    name: "Micronesia",
+    capital: "Palikir",
+    continent: "Asia"
+},
+{
+    name: "Moldova",
+    capital: "Chișinău",
+    continent: "Europe"
+},
+{
+    name: "Monaco",
+    capital: "Monaco",
+    continent: "Europe"
+},
+{
+    name: "Mongolia",
+    capital: "Ulaanbaatar",
+    continent: "Asia"
+},
+{
+    name: "Montenegro",
+    capital: "Podgorica",
+    continent: "Europe"
+},
+{
+    name: "Morocco",
+    capital: "Rabat",
+    continent: "Africa"
+},
+{
+    name: "Mozambique",
+    capital: "Maputo",
+    continent: "Africa"
+},
+{
+    name: "Myanmar",
+    capital: "Naypyidaw",
+    continent: "Asia"
+},
+{
+    name: "Namibia",
+    capital: "Windhoek",
+    continent: "Africa"
+},
+{
+    name: "Nepal",
+    capital: "Katmandu",
+    continent: "Asia"
+},
+{
+    name: "Netherlands",
+    capital: "Amsterdam",
+    continent: "Europe"
+},
+{
+    name: "New Zealand",
+    capital: "Wellington",
+    continent: "Oceania"
+},
+{
+    name: "Nicaragua",
+    capital: "Managua",
+    continent: "North America"
+},
+{
+    name: "Niger",
+    capital: "Niamey",
+    continent: "Africa"
+},
+{
+    name: "Nigeria",
+    capital: "Abuja",
+    continent: "Africa"
+},
+{
+    name: "North Macedonia",
+    capital: "Skopje",
+    continent: "Europe"
+},
+{
+    name: "Norway",
+    capital: "Oslo",
+    continent: "Europe"
+},
+{
+    name: "Oman",
+    capital: "Muscat",
+    continent: "Asia"
+},
+{
+    name: "Pakistan",
+    capital: "Islamabad",
+    continent: "Asia"
+},
+{
+    name: "Palau",
+    capital: "Ngerulmud",
+    continent: "Asia"
+},
+{
+    name: "Panama",
+    capital: "Panama City",
+    continent: "North America"
+},
+{
+    name: "Papua New Guinea",
+    capital: "Port Moresby",
+    continent: "Asia"
+},
+{
+    name: "Paraguay",
+    capital: "Asunción",
+    continent: "South America"
+},
+{
+    name: "Peru",
+    capital: "Lima",
+    continent: "South America"
+},
+{
+    name: "Philippines",
+    capital: "Manila",
+    continent: "Asia"
+},
+{
+    name: "Poland",
+    capital: "Warsaw",
+    continent: "Europe"
+},
+{
+    name: "Portugal",
+    capital: "Lisbon",
+    continent: "Europe"
+},
+{
+    name: "Qatar",
+    capital: "Doha",
+    continent: "Asia"
+},
+{
+    name: "Romania",
+    capital: "Bucharest",
+    continent: "Europe"
+},
+{
+    name: "Russia",
+    capital: "Moscow",
+    continent: "Europe"
+},
+{
+    name: "Rwanda",
+    capital: "Kigali",
+    continent: "Africa"
+},
+{
+    name: "St. Kittis and Nevis",
+    capital: "Basseterre",
+    continent: "North Ameica"
+},
+{
+    name: "Saint Lucia",
+    capital: "Castries",
+    continent: "North America"
+},
+{
+    name: "Samoa",
+    capital: "Apia",
+    continent: "Oceania"
+},
+{
+    name: "San Marino",
+    capital: "San Marino",
+    continent: "Europe"
+},
+{
+    name: "São Tomé and Príncipe",
+    capital: "São Tomé",
+    continent: "Africa"
+},
+{
+    name: "Saudi Arabia",
+    capital: "Riyadh",
+    continent: "Asia"
+},
+{
+    name: "Senegal",
+    capital: "Dakar",
+    continent: "Africa"
+},
+{
+    name: "Serbia",
+    capital: "Belgrade",
+    continent: "Europe"
+},
+{
+    name: "Seychelles",
+    capital: "Victoria",
+    continent: "Africa"
+},
+{
+    name: "Sierra Leone",
+    capital: "Freetown",
+    continent: "Africa"
+},
+{
+    name: "Singapore",
+    capital: "Singapore",
+    continent: "Asia"
+},
+{
+    name: "Slovakia",
+    capital: "Bratislava",
+    continent: "Europe"
+},
+{
+    name: "Slovenia",
+    capital: "Ljubljana",
+    continent: "Europe"
+},
+{
+    name: "Solomon Islands",
+    capital: "Honiara",
+    continent: "Oceania"
+},
+{
+    name: "Somalia",
+    capital: "Mogadishu",
+    continent: "Africa"
+},
+{
+    name: "South Africa",
+    capital: "Cape Town",
+    continent: "Africa"
+},
+{
+    name: "South Sudan",
+    capital: "Juba",
+    continent: ""
+},
+{
+    name: "Spain",
+    capital: "Madrid",
+    continent: "Europe"
+},
+{
+    name: "Sri Lanka",
+    capital: "Colombo",
+    continent: "Asia"
+},
+{
+    name: "Sudan",
+    capital: "Khartoum",
+    continent: "Africa"
+},
+{
+    name: "Suriname",
+    capital: "Paramaribo",
+    continent: "South America"
+},
+{
+    name: "Sweden",
+    capital: "Stockholm",
+    continent: "Europe"
+},
+{
+    name: "Switzerland",
+    capital: "Bern",
+    continent: "Europe"
+},
+{
+    name: "Syria",
+    capital: "Damascus",
+    continent: "Asia"
+},
+{
+    name: "Tajikistan",
+    capital: "",
+    continent: ""
+},
+{
+    name: "Tanzania",
+    capital: "Dushanbe",
+    continent: "Asia"
+},
+{
+    name: "Thailand",
+    capital: "Bangkok",
+    continent: "Asia"
+},
+{
+    name: "Togo",
+    capital: "Lomé",
+    continent: "Africa"
+},
+{
+    name: "Tonga",
+    capital: "Nukuʻalofa",
+    continent: "Oceania"
+},
+{
+    name: "Trinidad and Tobago",
+    capital: "Port of Spain",
+    continent: "South America"
+},
+{
+    name: "Tunisia",
+    capital: "Tunis",
+    continent: "Africa"
+},
+{
+    name: "Turkey",
+    capital: "Ankara",
+    continent: "Asia, Europe"
+},
+{
+    name: "Turkmenistan",
+    capital: "Ashgabat",
+    continent: "Asia"
+},
+{
+    name: "Tuvalu",
+    capital: "Funafuti",
+    continent: "Oceania"
+},
+{
+    name: "Uganda",
+    capital: "Kampala",
+    continent: "Africa"
+},
+{
+    name: "Ukraine",
+    capital: "Kyiv",
+    continent: "Europe"
+},
+{
+    name: "United Arab Emirates",
+    capital: "Abu Dhabi",
+    continent: "Asia"
+},
+{
+    name: "Scotland",
+    capital: "Edinburgh",
+    continent: "Europe"
+},
+{
+    name: "Wales",
+    capital: "Cardiff",
+    continent: "Europe"
+},
+{
+    name: "United Kingdom",
+    capital: "London",
+    continent: "Europe"
+},
+{
+    name: "United States",
+    capital: "Wahsington D.C.",
+    continent: "North America"
+},
+{
+    name: "Uruguay",
+    capital: "Montevideo",
+    continent: "South America"
+},
+{
+    name:  "Uzbekistan",
+    capital: "Tashkent",
+    continent: "Asia"
+},
+{
+    name: "Vanuatu",
+    capital: "Port Vila",
+    continent: "Oceania"
+},
+{
+    name: "Vatican City",
+    capital: "Vatican City",
+    continent: "Europe"
+},
+{
+    name: "Venezuela",
+    capital: "Caracas",
+    continent: "South America"
+},
+{
+    name: "Vietnam",
+    capital: "Hanoi",
+    continent: "Asia"
+},
+{
+    name: "Yemen",
+    capital: "Sana'a",
+    continent: "Asia"
+},
+{
+    name: "Zambia",
+    capital: "Lusaka",
+    continent: "Africa"
+},
+{
+    name: "Zimbabwe",
+    capital: "Harare",
+    continent: "Africa"
+},
+{
+    name: "Kosovo",
+    capital: "Pristina",
+    continent: "Europe"
+},
+{
+    name: "Taiwan",
+    capital: "Taipei",
+    continent: "Asia"
+}
+]
