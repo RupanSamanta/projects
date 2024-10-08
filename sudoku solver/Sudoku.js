406e67805923cb1b00b0a0a5bc84f1ff0f7c5bd7
Rupan Samanta <96118309+RupanSamanta@users.noreply.github.com>
2024-10-08 18:57:02 +0530

    Create Sudoku.js

---
+++ sudoku solver/Sudoku.js
@@ -0,0 +1,67 @@
+class Sudoku {
+   constructor(board) {
+      this.board = board;
+   }
+   isValid(num, row, col) {
+      for (let i=0; i<9; i++) {
+         if (this.board[i][col] == num) {
+            return false;
+         }
+      }
+      for (let i=0; i<9; i++) {
+         if (this.board[row][i] == num) {
+            return false;
+         }
+      }
+      let r = parseInt(row / 3) * 3;
+      let c = parseInt(col / 3) * 3;
+      for (let i=r; i<r+3; i++) {
+         for (let j=c; j<c+3; j++) {
+            if (this.board[i][j] == num) {
+               return false;
+            }
+         }
+      }
+      return true;
+   }
+   isBoardValid() {
+      for (let i=0; i<9; i++) {
+         for (let j=0; j<9; j++) {
+            let num = this.board[i][j];
+            if (num != 0) {
+               this.board[i][j] = 0;
+               if (!this.isValid(num, i, j)) {
+                  return false;
+               }
+               this.board[i][j] = num;
+            }
+         }
+      }
+      return true;
+   }
+   solve() {
+      if (!this.isBoardValid()) {
+         return false;
+      }
+      for (let i=0; i<9; i++) {
+         for (let j=0; j<9; j++) {
+            if (this.board[i][j] == 0) {
+               for (let k=1; k<=9; k++) {
+                  if (this.isValid(k, i, j)) {
+                     this.board[i][j] = k;
+                     if (this.solve()) {
+                        return true;
+                     }
+                     this.board[i][j] = 0;
+                  }
+               }
+               return false;
+            }
+         }
+      }
+      return true;
+   }
+   getBoard() {
+      return this.board;
+   }
+}
